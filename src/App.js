efa15f07766bd800efd1c26b7739cd54119df971
Joan Mira <JOAN.MIRA@VODAFONE.COM>
2018-12-23 11:14:54 +0100

    pieces there

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ class App extends Component {
     return (
       <div className="App">
         <div className="puzzle">
-          <div className="piece one" />
+          <div className="piece one" style={{ backgroundPosition: '0 0' }} />
           <div className="piece two" style={{ backgroundPosition: '-165px 0' }} />
           <div className="piece three" style={{ backgroundPosition: '-330px 0' }} />
-          <div className="piece four" />
-          <div className="piece five" />
-          <div className="piece six" />
-          <div className="piece seven" />
-          <div className="piece eight" />
+          <div className="piece four" style={{ backgroundPosition: '0 -165px' }} />
+          <div className="piece five" style={{ backgroundPosition: '-165px -165px' }} />
+          <div className="piece six" style={{ backgroundPosition: '-330px -165px' }} />
+          <div className="piece seven" style={{ backgroundPosition: '0 -330px' }} />
+          <div className="piece eight" style={{ backgroundPosition: '-165px -330px' }} />
+          <div className="piece nine" style={{ backgroundPosition: '-330px -330px' }} />
         </div>
 
         <img className="preview" src={monks} alt="Preview" width="100" />
