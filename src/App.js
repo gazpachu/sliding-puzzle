a636799f21d18764031ab5358cad409221ef85ff
Joan Mira <JOAN.MIRA@VODAFONE.COM>
2018-12-23 11:08:57 +0100

    testing pieces bg position

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import monks from './monks.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="puzzle">
+          <div className="piece one" />
+          <div className="piece two" style={{ backgroundPosition: '-165px 0' }} />
+          <div className="piece three" style={{ backgroundPosition: '-330px 0' }} />
+          <div className="piece four" />
+          <div className="piece five" />
+          <div className="piece six" />
+          <div className="piece seven" />
+          <div className="piece eight" />
+        </div>
+
+        <img className="preview" src={monks} alt="Preview" width="100" />
       </div>
     );
   }
