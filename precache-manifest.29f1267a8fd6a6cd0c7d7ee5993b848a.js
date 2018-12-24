5803c81f456e56565eb817a0ff310f466f3f8049
Joan Mira <jmira@sapient.com>
2018-12-24 17:58:51 +0100

    Updates

---
+++ precache-manifest.29f1267a8fd6a6cd0c7d7ee5993b848a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e32b9a7c985d89852028",
+    "url": "/sliding-puzzle/static/css/main.cc8a9ab5.chunk.css"
+  },
+  {
+    "revision": "e32b9a7c985d89852028",
+    "url": "/sliding-puzzle/static/js/main.e32b9a7c.chunk.js"
+  },
+  {
+    "revision": "768e7a0466a664e6ced2",
+    "url": "/sliding-puzzle/static/js/1.768e7a04.chunk.js"
+  },
+  {
+    "revision": "75ca8af8ac301df996fb",
+    "url": "/sliding-puzzle/static/js/runtime~main.75ca8af8.js"
+  },
+  {
+    "revision": "addcee8c7a2b15f412f38819656c4dea",
+    "url": "/sliding-puzzle/static/media/monks.addcee8c.jpg"
+  },
+  {
+    "revision": "61cb01d20a62f543dbd911ae7b4108e9",
+    "url": "/sliding-puzzle/static/media/logo.61cb01d2.svg"
+  },
+  {
+    "revision": "51d2f5b9af7e8794715de12d590fcee3",
+    "url": "/sliding-puzzle/static/media/smoke.51d2f5b9.png"
+  },
+  {
+    "revision": "5ad5e30e61f6207497db76615a129fd5",
+    "url": "/sliding-puzzle/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sliding-puzzle/precache-manifest.29f1267a8fd6a6cd0c7d7ee5993b848a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sliding-puzzle/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
