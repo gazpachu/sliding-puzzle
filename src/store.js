8d777d4b1c373e1c0cd6f502c85beefa92960348
Joan Mira <jmira@sapient.com>
2018-12-25 00:05:36 +0100

    fix for redux devtools

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import rootReducer from './reducers/rootReducer';
 export default function configureStore(initialState={}) {
   return createStore(
     rootReducer,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk), window.navigator.userAgent.includes('Chrome') ?
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose)
   );
 }
